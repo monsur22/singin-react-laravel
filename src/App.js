60103f839737fda6b7d648b80f46ceb991236c14
monsur22 <monsurahmedshafiq@gmail.com>
2021-12-21 20:35:10 +0600

    insall pakages and create pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {Fragment} from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useRoutes,
+} from "react-router-dom";
+import Home from './pages/Home';
+import Singin from './pages/Singin';
+import Singup from './pages/Singup'; 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     <Routes>
+
+            <Route path="/" element={<Home/>}/>
+            <Route path="/Singin" element={<Singin/>}/>
+            <Route path="/Singup" element={<Singup/>}/>
+
+
+
+      </Routes>
     </div>
   );
 }
