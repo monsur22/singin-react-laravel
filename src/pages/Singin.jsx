c6288892aadf21639a8a017a5b4717b6ed739ab3
monsur22 <monsurahmedshafiq@gmail.com>
2021-12-21 22:51:00 +0600

    singin generate jwt token and store in local storage

--- src/pages/Singin.jsx
+++ src/pages/Singin.jsx
@@ -1,20 +1,49 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import {
     Form,
     Button,
 
 } from 'react-bootstrap'
-const Singin = () => {
+const Singin =  () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const submitHandler = (e) =>{
+        e.preventDefault()
+        console.log(email, password)
+        // const config ={
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     }
+
+        // }
+        // const data =  axios.post('http://localhost:8000/api/login',{email, password, config})
+        // console.log(data)
+        // localStorage.setItem('userInfo', JSON.stringify(data))
+
+      axios.post('http://localhost:8000/api/login', {email,password})
+      .then(response => {
+        // if (response.data.accessToken) {
+          localStorage.setItem("userInfo", JSON.stringify(response.data.access_token));
+        // }
+
+        // return response.data;
+        console.log(response.data.access_token)
+      });
+
+    }
+
     return (
         <div className='container'>
-            <form>
+            <form onSubmit={submitHandler}>
                 <div className="form-group mt-3">
                     <label htmlFor="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"autoComplete='off' />
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"autoComplete='off' onChange={(e) => setEmail(e.target.value)} />
                 </div>
                 <div className="form-group mt-3">
                     <label htmlFor="exampleInputPassword1">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" autoComplete='off'/>
+                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" autoComplete='off'onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 <button type="submit" className="btn btn-primary mt-2">Submit</button>
             </form>
